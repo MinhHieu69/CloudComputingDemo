fff35b2fd8bba8eff5eaaf92da31acd38065dc0e
MinhHieu69 <hieupmgch18622@fpt.edu.vn>
2020-05-20 10:51:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,26 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Quang's CV</title>
+	<!-- Load font awesome -->
+	<link rel="stylesheet" href="assets/vendors/font-awesome-4.7.0/css/font-awesome.min.css">
+ 
+	<!-- Load google font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,700" rel="stylesheet">
+ 
+	<!-- Load main stylesheet -->
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
 <body>
-	
-<title>Index</title>	
-<h1>My first PHP page</h1>
-
-<?php
-echo "Wednesday !!!@@@";
-echo "Cloud Computing";
-echo "Change in fixbug branch";
-?>
-
+	<div class="wrapper flex">
+		<div class="sidebar">
+			
+			
+		</div><!-- End .sidebar -->
+		<div class="content">
+			
+		</div><!-- End .content -->
+	</div><!-- End .wrapper -->
 </body>
 </html>
